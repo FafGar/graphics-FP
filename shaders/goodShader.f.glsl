9a8b26ff3d276e7cbd2498f66746af030561c37d
The3DPrintist <41241889+The3DPrintist@users.noreply.github.com>
2023-12-02 19:25:38 -0700

    made shaders toony

--- shaders/goodShader.f.glsl
+++ shaders/goodShader.f.glsl
@@ -19,35 +19,65 @@ layout(location = 5) in vec3 badSpotHalfwayVec;
 // outputs
 out vec4 fragColorOut;// color to apply to this fragment
 
+vec3 rgb2hsv(vec3 c)
+{
+    vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+    vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+    vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+
+    float d = q.x - min(q.w, q.y);
+    float e = 1.0e-10;
+    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+}
+
+vec3 hsv2rgb(vec3 c)
+{
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
 void main() {
     // if we are looking at the front face of the fragment
-    if(gl_FrontFacing) {
-        vec3 newNormal = normalize(normalVec);
+    vec3 newNormal = normalize(normalVec);
 
-        vec3 dirLightVec = normalize(badDirLightVec);
-        vec3 spotLightVec = normalize(badSpotLightVec);
-        vec3 dirHalfwayVec = normalize(badDirHalfwayVec);
-        vec3 spotHalfwayVec = normalize(badSpotHalfwayVec);
-        // dirHalfwayVec = normalize(dirLightVec + eyePos);
-        // spotHalfwayVec = normalize(spotLightVec + eyePos);
+    vec3 dirLightVec = normalize(badDirLightVec);
+    vec3 spotLightVec = normalize(badSpotLightVec);
+    vec3 dirHalfwayVec = normalize(badDirHalfwayVec);
+    vec3 spotHalfwayVec = normalize(badSpotHalfwayVec);
+    // dirHalfwayVec = normalize(dirLightVec + eyePos);
+    // spotHalfwayVec = normalize(spotLightVec + eyePos);
 
-        float specAngle = max(dot(dirHalfwayVec, newNormal), 0.0);
-        float specular = pow(specAngle, materialShininess);
+    float specAngle = max(dot(dirHalfwayVec, newNormal), 0.0);
+    float specular = pow(specAngle, materialShininess);
 
-        vec3 diffColor = lightColor  * max( dot(newNormal, dirLightVec), 0.0 );
+    vec3 diffColor = lightColor  * max( dot(newNormal, dirLightVec), 0.0 );
 
-        if(1==1){ //Use this for spot light stuff later
-            specAngle = max(dot(spotHalfwayVec, newNormal), 0.0);
-            specular += pow(specAngle, materialShininess);
+    if(1==1){ //Use this for spot light stuff later
+        specAngle = max(dot(spotHalfwayVec, newNormal), 0.0);
+        specular += pow(specAngle, materialShininess);
 
-            diffColor += lightColor  * max( dot(newNormal, spotLightVec), 0.0 );
-        }
-        
-        fragColorOut = vec4(materialAmbColor+
-                            materialDiffColor * diffColor+
-                            materialSpecColor * specular ,1.0
-                            );
-    }    else {
-        fragColorOut = vec4(0);
+        diffColor += lightColor  * max( dot(newNormal, spotLightVec), 0.0 );
     }
+
+    vec3 shadedColor = materialAmbColor+materialDiffColor * diffColor+materialSpecColor * specular;
+
+    vec3 shadedHSV = rgb2hsv(shadedColor);
+
+    vec3 toonHSV = shadedHSV;
+    float toonV = toonHSV.z;
+
+    int accuracy = 8;
+
+    toonV = pow(toonV, 0.5);
+    toonV *= accuracy;
+    toonV = floor(toonV);
+    toonV = toonV/accuracy;
+    toonV = pow(toonV, 2);
+
+    toonHSV.z = toonV;
+
+    vec3 toonColor = hsv2rgb(toonHSV);
+
+    fragColorOut = vec4(toonColor ,1.0);
 }
--- shaders/textureShader.f.glsl
+++ shaders/textureShader.f.glsl
@@ -12,11 +12,47 @@ in vec2 texCoord;
 // fragment outputs
 out vec4 fragColorOut;
 
+vec3 rgb2hsv(vec3 c)
+{
+    vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+    vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+    vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+
+    float d = q.x - min(q.w, q.y);
+    float e = 1.0e-10;
+    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+}
+
+vec3 hsv2rgb(vec3 c)
+{
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
 void main() {
     // TODO #F - get texel
     vec4 texel = texture(texMap, texCoord);
 
+    vec3 shadedColor = vec3(texel.x,texel.y,texel.z);
 
-    // TODO #G - set texel
-    fragColorOut = texel;
+
+    vec3 shadedHSV = rgb2hsv(shadedColor);
+
+    vec3 toonHSV = shadedHSV;
+    float toonV = toonHSV.z;
+
+    int accuracy = 8;
+
+    toonV = pow(toonV, 0.5);
+    toonV *= accuracy;
+    toonV = floor(toonV);
+    toonV = toonV/accuracy;
+    toonV = pow(toonV, 2);
+
+    toonHSV.z = toonV;
+
+    vec3 toonColor = hsv2rgb(toonHSV);
+
+    fragColorOut = vec4(toonColor ,1.0);
 }
